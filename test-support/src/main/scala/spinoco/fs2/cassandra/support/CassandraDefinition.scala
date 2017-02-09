e46c0f5df5a7d0729d65bbe458ee57a9dc896f26
Pavel Chlupacek <pavel.chlupacek@spinoco.com>
2017-02-09 15:12:47 +0100

    Added 3.9 reelase test

--- test-support/src/main/scala/spinoco/fs2/cassandra/support/CassandraDefinition.scala
+++ test-support/src/main/scala/spinoco/fs2/cassandra/support/CassandraDefinition.scala
@@ -56,14 +56,20 @@ object CassandraDefinition {
       , allKeySpaceQuery = system.schema.queryAllKeySpaces.map(_.keyspace_name)
     )
 
+  val `3.9`:CassandraDefinition =
+    latest.copy(
+      tag = "3.9"
+      , allKeySpaceQuery = system.schema.queryAllKeySpaces.map(_.keyspace_name)
+    )
+
   lazy val All:Seq[CassandraDefinition] = Seq(
-    `2.1`, `2.2`, `3.0`, `3.5`, `3.7`
+    `2.1`, `2.2`, `3.0`, `3.5`, `3.7`, `3.9`
   )
 
 
   implicit class CassandraDefinitionSnytax (val self: CassandraDefinition) extends AnyVal {
     def isV3Compatible:Boolean = {
-      self == `3.0` || self == `3.5` || self == `3.7`
+      self == `3.0` || self == `3.5` || self == `3.7` || self = `3.9`
     }
   }
 

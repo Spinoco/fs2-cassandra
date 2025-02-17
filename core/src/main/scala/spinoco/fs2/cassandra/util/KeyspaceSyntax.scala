316913d23ebd13a0eb3a0d12e9e69284c508ede5
Karel Tucek <kareltucek@centrum.cz>
2025-02-17 15:09:18 +0100

    Fill in comments.

--- core/src/main/scala/spinoco/fs2/cassandra/util/KeyspaceSyntax.scala
+++ core/src/main/scala/spinoco/fs2/cassandra/util/KeyspaceSyntax.scala
@@ -4,6 +4,7 @@ import com.datastax.oss.driver.api.core.CqlSession
 import com.datastax.oss.driver.api.core.metadata.schema.KeyspaceMetadata
 import spinoco.fs2.cassandra.util.ToOptionSyntax.OptionalConverter
 
+/** Syntax for getting keyspace metadata from a CqlSession. */
 object KeyspaceSyntax {
   implicit class GetKeyspaceMetadataSyntax(val self: CqlSession) extends AnyVal {
     def getKeyspaceMetadata(keyspaceName: String): Option[KeyspaceMetadata] = {
--- core/src/main/scala/spinoco/fs2/cassandra/util/ToOptionSyntax.scala
+++ core/src/main/scala/spinoco/fs2/cassandra/util/ToOptionSyntax.scala
@@ -2,6 +2,9 @@ package spinoco.fs2.cassandra.util
 
 import java.util.Optional
 
+/**
+  * Provides syntax to convert Java's Optional to Scala's Option.
+  */
 object ToOptionSyntax {
   implicit class OptionalConverter[T](val optional: Optional[T]) extends AnyVal {
     def toOption: Option[T] = if (optional.isPresent) Some(optional.get()) else None
